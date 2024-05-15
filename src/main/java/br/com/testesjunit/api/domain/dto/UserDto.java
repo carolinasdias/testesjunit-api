ea749623bc2fc8e1a330b9b73e26431e7eef864a
carolinasdias <carolinasdias06@gmail.com>
2024-05-15 11:37:23 -0300

    class user dto

---
+++ src/main/java/br/com/testesjunit/api/domain/dto/UserDto.java
@@ -0,0 +1,19 @@
+package br.com.testesjunit.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class UserDto {
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
