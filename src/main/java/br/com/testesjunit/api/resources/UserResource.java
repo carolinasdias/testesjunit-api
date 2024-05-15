202751c0a1f13ccc1d1b766a83e54720cbedc267
carolinasdias <carolinasdias06@gmail.com>
2024-05-15 13:30:18 -0300

    endpoint findAll para user

--- src/main/java/br/com/testesjunit/api/resources/UserResource.java
+++ src/main/java/br/com/testesjunit/api/resources/UserResource.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/user")
 
@@ -25,5 +28,11 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDto> findById(@PathVariable Integer id) {
         return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDto.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> findAll() {
+        return ResponseEntity.ok().body(service.findAll()
+                .stream().map(x -> mapper.map(x, UserDto.class)).collect(Collectors.toList()));
     }
 }
--- src/main/java/br/com/testesjunit/api/services/UserService.java
+++ src/main/java/br/com/testesjunit/api/services/UserService.java
@@ -2,8 +2,11 @@ package br.com.testesjunit.api.services;
 
 import br.com.testesjunit.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 
 }
