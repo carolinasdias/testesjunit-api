c5b4bf9055d0691686eb7b9c18d1bf92499f8239
carolinasdias <carolinasdias06@gmail.com>
2024-05-15 11:22:31 -0300

    tratamento de excecao para objeto nao encontrado

--- src/main/java/br/com/testesjunit/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/testesjunit/api/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.testesjunit.api.services.impl;
 import br.com.testesjunit.api.domain.User;
 import br.com.testesjunit.api.repositories.UserRepository;
 import br.com.testesjunit.api.services.UserService;
+import br.com.testesjunit.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
